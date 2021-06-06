f745e34d0ad0f6df42c79d031ca376a7c5f52126
Win <winzaza@msn.com>
2021-06-06 11:27:04 +0700

    add swagger fastify Node 12 / 14.17.0 fastify not work

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,9 +5,24 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 const port = process.env.PORT || 4000
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, { cors: true });
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule,
+    new FastifyAdapter()
+    , { cors: true });
+
+    const config = new DocumentBuilder()
+    .setTitle('App example')
+    .setDescription(' API description')
+    .setVersion('1.0')
+    .addTag('app')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   app.useGlobalPipes(new ValidationPipe({
     transform: true
   }));
