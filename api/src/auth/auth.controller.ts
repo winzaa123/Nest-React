fbadfe46c990df9aaa4f3d0215616df067e2fa75
Win <winzaza@msn.com>
2021-10-06 10:49:54 +0700

    fix path

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Request, Post, UseGuards, Body, Get } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
-import {LocalAuthGuard} from "./local-auth.guard"
+import { LocalAuthGuard } from "./local-auth.guard"
 
 import LoginDto from "./dto/LoginDto"
 import AuthService from "./auth.service"
 import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { User } from 'src/user/user.entity';
+import { User } from '../user/user.entity';
 
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { JwtPayload } from './interfaces/JwtPayload';
@@ -17,49 +17,49 @@ import { RolesGuard } from './roles.guard';
 @Controller('auth')
 export class AuthController {
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) { }
 
-    // @UseGuards(LocalAuthGuard)
-    @Post('login')
-    @ApiResponse({ status: 400, description: 'Bad Request.'  })
-    async login(@Body() credential: LoginDto) {
-       
-      return await this.authService.validateUser(credential)
-    }
+  // @UseGuards(LocalAuthGuard)
+  @Post('login')
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
+  async login(@Body() credential: LoginDto) {
 
-    @Post('signup')
-    @ApiResponse({
-      status: 200,
-      description: 'User Data',
-      type: User,
-    })
-    async signup(@Body() credential: LoginDto) {
-       
-      return await this.authService.signup(credential)
-    }
+    return await this.authService.validateUser(credential)
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Get('profile')
-    @ApiBearerAuth()
-    getProfile(@Request() req) {
-      console.log( req.user)
-      return this.authService.getUser(req.user.userId)
-    }
+  @Post('signup')
+  @ApiResponse({
+    status: 200,
+    description: 'User Data',
+    type: User,
+  })
+  async signup(@Body() credential: LoginDto) {
 
-    @Get('whoami')
-    @UseGuards(AuthGuard())
-    @ApiBearerAuth()
-    public async testAuth(@Request() req: any): Promise<JwtPayload> {
-      return req.user;
-    }
+    return await this.authService.signup(credential)
+  }
 
-    @Get('testRole')
-    @UseGuards(AuthGuard(),RolesGuard)
-    @Roles('admin','xxx')
-    @ApiBearerAuth()
-    async create( ) {
-      return "can access"
-    }
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  @ApiBearerAuth()
+  getProfile(@Request() req) {
+    console.log(req.user)
+    return this.authService.getUser(req.user.userId)
+  }
 
-    
+  @Get('whoami')
+  @UseGuards(AuthGuard())
+  @ApiBearerAuth()
+  public async testAuth(@Request() req: any): Promise<JwtPayload> {
+    return req.user;
+  }
+
+  @Get('testRole')
+  @UseGuards(AuthGuard(), RolesGuard)
+  @Roles('admin', 'xxx')
+  @ApiBearerAuth()
+  async create() {
+    return "can access"
+  }
+
+
 }
